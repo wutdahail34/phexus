0537fe1a9f0f6efe7de38d10f599d01abb20dbf6
ph4xus <168146698+ph4xus@users.noreply.github.com>
2025-02-28 17:49:09 -0800

    Update fetchington.js

--- gxmes/js/fetchington.js
+++ gxmes/js/fetchington.js
@@ -278,34 +278,10 @@ document.addEventListener("DOMContentLoaded", function () {
                     <i class="fas fa-expand"></i>
                 </button>
             </div>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3858578074050552"
-                 crossorigin="anonymous"></script>
-            <!-- footer -->
-            <ins class="adsbygoogle"
-                 style="display:block"
-                 data-ad-client="ca-pub-3858578074050552"
-                 data-ad-slot="3817988366"
-                 data-ad-format="auto"
-                 data-full-width-responsive="true"></ins>
-            <script>
-                 (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
             <div class="recommended-games" data-nosnippet>
                 <h3>Recommended Games</h3>
                 <div class="games-list" id="recommendedGames"></div>
             </div>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3858578074050552"
-                 crossorigin="anonymous"></script>
-            <!-- footer -->
-            <ins class="adsbygoogle"
-                 style="display:block"
-                 data-ad-client="ca-pub-3858578074050552"
-                 data-ad-slot="3817988366"
-                 data-ad-format="auto"
-                 data-full-width-responsive="true"></ins>
-            <script>
-                 (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
             <div class="keywords-section">
                 <div class="keywords">
                     <h3>Keywords:</h3>
